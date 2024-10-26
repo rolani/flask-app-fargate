bc77f601b98451d38f96be977d381378ebbd197f
Richard Olaniyan <34588531+rolani@users.noreply.github.com>
2024-10-26 17:23:31 +0000

    flask app

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use the official Python image as the base image
+FROM python:3.12-slim
+
+RUN apt-get update -y 
+
+# Set the working directory in the container
+WORKDIR /app
+
+RUN pip install flask
+
+# Copy the application code
+COPY app.py .
+
+# Expose port 5000 for Flask
+EXPOSE 80
+
+# Define the command to run the Flask app
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,24 @@
+"""Module providing random choices."""
+from random import choices
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+def random_fruit():
+    """Returns random fruit"""
+
+    fruits = ["apple", "cherry", "orange", "banana", "pawpaw", "grapes"]
+    return choices(fruits)
+
+
+@app.route("/")
+def fruit():
+    """Return random fruit"""
+
+    my_fruit = random_fruit()
+    return render_template("index.html", title="Random Fruit", fruit=my_fruit)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
